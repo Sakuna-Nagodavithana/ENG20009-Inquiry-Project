df2a727604450ca024d4a93406555c9d5456e904
Sakun Hemaka Nagodavithana <85131239+Sakuna-Nagodavithana@users.noreply.github.com>
2024-04-11 12:23:11 +1000

    First Five Command works

--- SDI-12-Object/SDI-12-Object.ino
+++ SDI-12-Object/SDI-12-Object.ino
@@ -18,79 +18,65 @@ private:
 
 public:
     void runCommand(char command[]) {
-        // char commandChars[command.length() + 1];
-        // command.toCharArray(commandChars, command.length() + 1);
+      Serial.println(command);
+      ms.Target(command);
 
-        // char buf[command.length()]; // +1 for the null terminator
-        // //memset(buf, 0, sizeof(buf)); // Initialize the buffer with zeroes
-        // //command.toCharArray(buf, command.length() + 1); // Ensure null termination
-        // Serial.println(command);
-        
-        // // Debugging: print each character in hex
-        // for (int i = 1; i < command.length(); i++) {
-        //   buf[i] = command[i];
-        //   Serial.println(buf[i]);
-        //   Serial.print("0x");
-        //   Serial.println(buf[i], HEX);
-        // }
-        
-        ms.Target(command);
+      // Address Query
+      if (ms.Match(".*?!") == REGEXP_MATCHED) {
+          writeToSDI12(String(address));
+      }
 
-        // Address Query
-        if (ms.Match(".*a!") == REGEXP_MATCHED) {
-            writeToSDI12(String(address));
-        }
+      // Change address
+      if (ms.Match(".*[0-9]A[0-9]!") == REGEXP_MATCHED) {
+          //Serial.println("Got in ");
+          int newAddress = command[0] - '0';
+          int newAddressLength = command[1] - '0';
 
-        // Change address
-        if (ms.Match("^[0-9A-Z]+[0-9A-Z]{1,2}") == REGEXP_MATCHED) {
-            int newAddress = command[0] - '0';
-            int newAddressLength = command[1] - '0';
+          if (newAddress == address) {
+              address = (newAddressLength == 2 ? (command[2] - '0') * 10 + (command[3] - '0') : command[2] - '0');
+              writeToSDI12(String(address));
+          } else {
+              writeToSDI12("The address that you provided is not the current address. You can find the current address by using this command (?!) ");
+          }
+      }
 
-            if (newAddress == address) {
-                address = (newAddressLength == 2 ? (command[2] - '0') * 10 + (command[3] - '0') : command[2] - '0');
-                writeToSDI12(String(address));
-            } else {
-                writeToSDI12("The address that you provided is not the current address. You can find the current address by using this command (?!)");
-            }
-        }
+      // Send Identification
+      if (ms.Match("^[0-9A-Z]+I!") == REGEXP_MATCHED) {
+          writeToSDI12(String(address));
+          writeToSDI12("14");
+          writeToSDI12("ENG2009");
+          writeToSDI12("104643522");
+      }
 
-        // Send Identification
-        if (ms.Match("^[0-9A-Z]+I!") == REGEXP_MATCHED) {
-            writeToSDI12(String(address));
-            writeToSDI12("14");
-            writeToSDI12("ENG2009");
-            writeToSDI12("104643522");
-        }
+      // Start Measurement
+      if (ms.Match("^[0-9A-Z]+M!") == REGEXP_MATCHED) {
+          bme.setTemperatureOversampling(BME680_OS_8X);
+          bme.setHumidityOversampling(BME680_OS_2X);
+          bme.setPressureOversampling(BME680_OS_4X);
+          bme.setIIRFilterSize(BME680_FILTER_SIZE_3);
+          bme.setGasHeater(320, 150); // 320*C for 150 ms
 
-        // Start Measurement
-        if (ms.Match("^[0-9A-Z]+M!") == REGEXP_MATCHED) {
-            bme.setTemperatureOversampling(BME680_OS_8X);
-            bme.setHumidityOversampling(BME680_OS_2X);
-            bme.setPressureOversampling(BME680_OS_4X);
-            bme.setIIRFilterSize(BME680_FILTER_SIZE_3);
-            bme.setGasHeater(320, 150); // 320*C for 150 ms
+          if (!initialize) {
+              bme.begin(0x76);
+              initialize = true;
+              writeToSDI12("Measurement Started");
+          } else {
+              writeToSDI12("You already initialized it");
+          }
+      }
 
-            if (!initialize) {
-                bme.begin(0x76);
-                initialize = true;
-                writeToSDI12("Measurement Started");
-            } else {
-                writeToSDI12("You already initialized it");
-            }
-        }
+      // Send Data
+      if (ms.Match("^[0-9A-Z]+D[0-9]!") == REGEXP_MATCHED) {
+          int commandAddress = command[0] - '0';
+          int sensorID = command[2] - '0';
 
-        // Send Data
-        if (ms.Match("^[0-9A-Z]+D[0-9]!") == REGEXP_MATCHED) {
-            int commandAddress = command[0] - '0';
-            int sensorID = command[2] - '0';
-
-            if (commandAddress != address) {
-                writeToSDI12("The address that you provided is not the current address. You can find the current address by using this command (?!)");
-            } else {
-                float value = getData(sensorID);
-                writeToSDI12(String(value));
-            }
-        }
+          if (commandAddress != address) {
+              writeToSDI12("The address that you provided is not the current address. You can find the current address by using this command (?!)");
+          } else {
+              float value = getData(sensorID);
+              writeToSDI12(String(value));
+          }
+      }
     }
 
     float getData(int sensorID) {
@@ -156,15 +142,18 @@ void loop() {
   if (Serial1.available()) {
     String input = Serial1.readStringUntil('\n'); // Read until newline character
     char buf[input.length()]; // Buffer to hold the incoming data
-    //memset(buf, 0, sizeof(buf)); // Initialize the buffer with zeroes
-    Serial.println(input);
+    memset(buf, 0, sizeof(buf)); // Initialize the buffer with zeroes
+
     // Copy the string into the buffer starting from the second character
     for (int i = 1; i < input.length(); i++) {
-      buf[i-1] = input[i];
+      buf[i - 1] = input[i];
     }
+    
+    // Print the received input for debugging
+    Serial.println(input);
 
+    // Debugging: print each character in hex starting from the first copied character
     for (int i = 0; i < input.length() - 1; i++) {
-      Serial.println(buf[i]);
       Serial.print("0x");
       Serial.println(buf[i], HEX);
     }
